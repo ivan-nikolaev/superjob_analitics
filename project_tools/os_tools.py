3ddcb26e7d91c8d1effbfc3409849259b5c95ca5
Ivan <60277@mail.ru>
2020-11-19 17:52:22 +0500

    modify extract_zip_by_vacancy

--- project_tools/os_tools.py
+++ project_tools/os_tools.py
@@ -12,11 +12,15 @@ def generator_files_in_dir(top_directory, extension=''):
                 yield filename_full
 
 
-def extract_zip_by_vacancy(input_zipfile):
+def extract_zip_by_vacancy(input_zipfile, n=-1):
     print(f"\nОткрываем zip файл: {input_zipfile}")
     with zipfile.ZipFile(input_zipfile) as opened_zip:
         for pickle_file_in_zip in tqdm(opened_zip.namelist()[:], desc="files in zip"):
             with opened_zip.open(pickle_file_in_zip, 'r') as mypicklefile:
                 block_vacancies = pickle.load(mypicklefile)
-                for vacancy in block_vacancies:
-                    yield vacancy
+                if n == -1:
+                    for vacancy in block_vacancies:
+                        yield vacancy
+                else:
+                    for vacancy in block_vacancies[:n]:
+                        yield vacancy
